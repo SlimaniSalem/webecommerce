45332b1db0491757fb9c8b173727f954c6d0f8db
SlimaniSalem <didihnon@gmail.com>
2023-11-26 21:38:24 +0100

    4.0

--- shop/urls.py
+++ shop/urls.py
@@ -4,10 +4,12 @@ from django.urls import path
 from store.views import index, detail_produit
 from django.conf.urls.static import static
 from shop import settings
+from user_cont.views import user_log
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    path('user/',user_log,name='user'),
     path('product/<str:slug>/', detail_produit, name="detail"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
